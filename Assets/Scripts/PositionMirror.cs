d7f4eea7a7f425e4783794dbecfe7f6efdd91bce
Benjamin Andersen <band818@UoA.auckland.ac.nz>
2017-07-03 08:45:34 +1200

    Add position mirroring to fix leap tracking issue

---
+++ Assets/Scripts/PositionMirror.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionMirror : MonoBehaviour {
+
+	public GameObject childObject;
+	public bool constrainX = false;
+	public bool constrainY = true;
+	public bool constrainZ = false;
+
+	// Use this for initialization
+	void Start () {
+		//Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 parentPosition = this.gameObject.transform.position;
+		Vector3 childPosition = childObject.transform.position;
+		if (constrainX) {
+			childPosition.x = parentPosition.x;
+		}
+		if (constrainY) {
+			childPosition.y = parentPosition.y;
+		}
+		if (constrainZ) {
+			childPosition.z = parentPosition.z;
+		}
+		childObject.transform.position = childPosition;
+	}
+}
