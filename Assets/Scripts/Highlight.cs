f4dae6e7bcb87ff5b5a540c2951cd5904f1a70cb
Arran Davis <adav194@UoA.auckland.ac.nz>
2017-07-20 20:32:55 +1200

    Implements simple selection and manipulation
    For more information try the desktop testing scene. A highlighted selection metaphor has been used to select objects and move them individually. The controller triggers are used for rotating the scene around its origin

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : Selectable {
+	public Material def;
+	public Material highlight;
+
+	public float speed = 10f;
+
+	public override void OnSelect () {
+		MeshRenderer rend = gameObject.GetComponent<MeshRenderer> ();
+		rend.material = highlight;
+	}
+
+	public override void WhileSelected () {
+		Vector2 rightJoy = Controller.GetInstance ().GetJoystickAxis (Controller.Joystick.Right);
+
+		Debug.Log (rightJoy);
+		if (rightJoy.y <= -Controller.deadZone || Controller.deadZone <= rightJoy.y) {
+			Debug.Log ("Moving");
+			Vector3 pos = transform.position;
+			pos.y += rightJoy.y * speed * Time.deltaTime;
+			transform.position = pos;
+		}
+			
+
+
+	}
+
+	public override void OnUnselect () {
+		MeshRenderer rend = gameObject.GetComponent<MeshRenderer> ();
+		rend.material = def;
+	}
+
+}
---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour {
+
+	public Selectable[] selectList;
+	public int firstSelected = 0;
+
+	public int selected;
+
+	public float coolDown = 2f;
+	private float counter = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+		if (selectList.Length <= 0) {
+			Debug.LogError ("Select List is not initialized, please initialize from Editor");
+		} else if (selectList.Length <= firstSelected) {
+			Debug.LogError ("Initial Select Index is larger than the total length of selectables");
+			selected = 0;
+		} else {
+			selected = firstSelected;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Controller controller = Controller.GetInstance ();
+
+		Vector2 leftStick = controller.GetJoystickAxis (Controller.Joystick.Left);
+
+		float vert = leftStick.y;
+		if (counter >= coolDown)
+			counter = 0f;
+
+		// Up
+		if (vert >= Controller.deadZone && counter == 0f) {
+			selectList [selected].Unselect ();
+			selected++;
+			if (selected >= selectList.Length)
+				selected = 0;
+			selectList [selected].Select ();
+			counter += Time.deltaTime;
+		} else if (vert >= Controller.deadZone) {
+			counter += Time.deltaTime;
+		}
+
+
+		// Dead zone
+		if (- Controller.deadZone < vert && vert < Controller.deadZone)
+			counter = 0f;
+		
+		// Down
+		if (vert <= - Controller.deadZone && counter == 0f) {
+			selectList [selected].Unselect ();
+			selected--;
+			if (selected < 0)
+				selected = selectList.Length - 1;
+			selectList [selected].Select ();
+			counter += Time.deltaTime;
+		} else if (vert <= - Controller.deadZone) {
+			counter += Time.deltaTime;
+		}
+
+	}
+}
