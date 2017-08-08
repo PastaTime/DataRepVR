1b770f2de4bf50c74a06357add6681233e686fa3
MisterAndersen <MisterAndersen@users.noreply.github.com>
2017-08-08 13:28:51 +1200

    Add support for rotation using the right joystick

--- Assets/Scripts/Desktop/Rotation.cs
+++ Assets/Scripts/Desktop/Rotation.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Rotation : MonoBehaviour {
+
+	public enum Direction
+	{
+		LEFT=1, RIGHT=-1
+	}
 
 	public float minSpeed = 10f;
 	public float maxSpeed = 100f;
@@ -33,8 +39,12 @@ public class Rotation : MonoBehaviour {
 		transform.Rotate (rotation);
 	}
 
-
-
+	public void rotate(Direction direction)
+	{
+		Vector3 rotation = Vector3.zero;
+		rotation.y = (float)direction * maxSpeed * Time.deltaTime;
+			transform.Rotate(rotation);
+	}
 
 	private float getSpeed() {
 		counter += Time.deltaTime;
