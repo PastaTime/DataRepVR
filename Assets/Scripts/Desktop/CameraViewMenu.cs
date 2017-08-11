8df6778e0dccb48af218e9b87548e867f47a4b16
MisterAndersen <MisterAndersen@users.noreply.github.com>
2017-08-11 17:54:57 +1200

    Remove further unused code

--- Assets/Scripts/Desktop/CameraViewMenu.cs
+++ Assets/Scripts/Desktop/CameraViewMenu.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraViewMenu : Menu {
-//	public Menu belowMenu;
 
 	public override void OnActivation () {
-//		Debug.Log ("Top Down Menu Selected");
 		manager.camPan.MoveTo (transform);
 	}
 }
