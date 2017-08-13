18bb60fda5a1b2aae6846aacc39fb783b4f49640
MisterAndersen <MisterAndersen@users.noreply.github.com>
2017-08-14 10:54:39 +1200

    Add ability to reset VR camera position using controller or keyboard

---
+++ Assets/CameraReposition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class CameraReposition : MonoBehaviour {
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.C) || Controller.GetInstance().GetButtonDown(Controller.Button.Start))
+		{
+			InputTracking.Recenter();
+		}
+	}
+}
