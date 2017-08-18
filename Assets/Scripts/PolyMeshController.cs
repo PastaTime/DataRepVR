9073ed2da74ed5a1c8ea47b6f80df3da6b5cbfcd
MisterAndersen <MisterAndersen@users.noreply.github.com>
2017-08-18 18:45:23 +1200

    Add sanity check for position mirorring

--- Assets/Scripts/PolyMeshController.cs
+++ Assets/Scripts/PolyMeshController.cs
@@ -105,6 +105,7 @@ public class PolyMeshController : MonoBehaviour {
 
 	public float getCornerVerticalOffset()
 	{
+		if (subMeshes.Count < 1 || subMeshes[0].getMesh() == null || subMeshes[0].getMesh().vertices.Length < 1) return 0;
 		return transform.position.y - transform.TransformPoint(subMeshes[0].getMesh().vertices[0]).y;
 	}
 
